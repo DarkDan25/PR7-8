d4c09d014898c08a412ba59296d3e92d123dd1b4
Zyablik <elektrosha88@gmail.com>
2024-12-05 10:49:04 +0300

    UI Test

--- app/src/androidTest/java/com/zyablik/pr7_8/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/zyablik/pr7_8/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.zyablik.pr7_8
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import android.os.Handler
+import android.os.Looper
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,42 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.zyablik.pr7_8", appContext.packageName)
+    fun checkUiElementsVisibility() {
+        onView(withId(R.id.imageView)).check(matches(isDisplayed()))
+        onView(withId(R.id.button)).check(matches(isDisplayed()))
+        onView(withId(R.id.txt)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun checkImageInitialState() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            onView(withId(R.id.imageView)).check(matches(null))},
+            2000)
+    }
+
+    @Test
+    fun checkText() {
+        onView(withId(R.id.txt)).check(matches(withText("Hello World!")))
+    }
+
+    @Test
+    fun checkButtonClick() {
+        onView(withId(R.id.button)).perform(click())
+        Handler(Looper.getMainLooper()).postDelayed({
+            checkNotNull(R.id.imageView)
+        }, 2000)
+    }
+
+    @Test
+    fun checkTextChangedDisplayAfterDownload() {
+
+        onView(withId(R.id.button)).perform(click())
+        Handler(Looper.getMainLooper()).postDelayed({
+            onView(withId(R.id.txt)).check(matches(withText("Downloaded")))
+        }, 3000)
     }
 }
